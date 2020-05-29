9ab4b11259795c99c4c4dbd0e0992aa8e8aa996f
ahkaneti <aaronkaneti2020@u.northwestern.edu>
2020-05-29 22:54:46 +0300

    Grammar mistakes

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -12,9 +12,9 @@ function AboutMe(){
       <div className="midpart">
         <h1>About <strong>Me</strong></h1>
         <h3>Bachelor of Science in Psychology & Computer Science • Front-End Engineer • Data Analyst</h3>
-        <p><strong>I am a software engineer that strives to create the most ergonomically induced code as possible. 
+        <p><strong>I am a software engineer that strives to create code that is as ergonomically induced as possible.
           My aim, when coding, is to create code that anyone can use.</strong></p>
-        <h1>Confidence in <strong>my Technical Expertise</strong></h1>
+        <h1><strong>Technical Expertise</strong></h1>
         <div className="expertise-row">
           <div className="all-parts">
             <p className='subject'><strong>HTML 5</strong></p>
@@ -55,24 +55,24 @@ function AboutMe(){
           <div className="all-parts">
             <p className='subject'><strong>Python</strong></p>
             <p className='percentage'>85%</p>
-            <div className="total"><div className="achieved"></div></div>
+            <div className="total"><div className="achieved" style={{ width: "85%" }}></div></div>
           </div>
           <div className="all-parts">
             <p className='subject'><strong>C#</strong></p>
             <p className='percentage'>70%</p>
-            <div className="total"><div className="achieved"></div></div>
+            <div className="total"><div className="achieved" style={{ width: "70%" }}></div></div>
           </div>
         </div>
         <div className="expertise-row">
           <div className="all-parts">
             <p className='subject'><strong>CSS3</strong></p>
             <p className='percentage'>80%</p>
-            <div className="total"><div className="achieved"></div></div>
+            <div className="total"><div className="achieved" style={{ width: "80%" }}></div></div>
           </div>
           <div className="all-parts">
             <p className='subject'><strong>Racket</strong></p>
             <p className='percentage'>90%</p>
-            <div className="total"><div className="achieved"></div></div>
+            <div className="total"><div className="achieved" ></div></div>
           </div>
         </div>
       </div>
